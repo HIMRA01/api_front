fdcb951d896e2b26ac639723a2dae5c3566e09fc
RAHIM <DOSSORAHIM2018@GMAIL.COM>
2023-09-27 16:57:04 +0200

    2

--- structure.js
+++ structure.js
@@ -1,6 +1,6 @@
 const REGISTERFORM = $("#registerFORM");
 const LOGINFORM = $("#loginFORM");
-
+getUserList();
 REGISTERFORM.on("submit", (e) => {
   e.preventDefault();
   let pseudo = $("#pseudo").val();
@@ -63,12 +63,93 @@ function login(pseudo, password, action) {
       response
         .json()
         .then((data) => {
-            console.log(data);
-            localStorage.setItem("iduser", data.data.id)
+          console.log(data);
+          localStorage.setItem("iduser", data.data.id);
           window.location.href("");
         })
         .catch((error) => error);
     })
 
     .catch((error) => console.log("il ya une erreur"));
+}
+
+// fonction pour obtenir la liste des utilisateur
+function getUserList() {
+  fetch("http://localhost/api_back/getuserlist/")
+    .then((response) => {
+      response
+        .json()
+        .then((data) => {
+          // console.log(data);
+          printUsers(data.data);
+        })
+        .catch((error) => console.log(error));
+    })
+    .catch((error) => console.log(error));
+}
+
+// fonction pour afficher la liste des utilisateur
+function printUsers(listUser) {
+  listUser.forEach((element) => {
+    // creer une balise p en lui ajoutant le prenom de l'utilisateur comme texte
+    // let p = $("p").append(element.firstname);
+    let p = document.createElement("p");
+    p.textContent = element.firstname;
+    p.id = element.id_user;
+    p.addEventListener("click", () => {
+      getListMessage(localStorage.getItem("iduser"), p.id);
+    });
+    // on ajoute le paragraphe comme enfant de la div avec la class user_list
+    $("#user_list").append(p);
+  });
+
+  // fonction pour avoir la liste des messages entre 2 utilisateurs getMessage
+}
+
+function getListMessage(expeditor, receiver) {
+  fetch(
+    "http://localhost/api_back/getListMessage/" + expeditor + "/" + receiver
+  )
+    .then((response) => {
+      response
+        .json()
+
+        .then((data) => {
+          // console.log(data);
+
+          // appel de printUsers
+
+          // printMessages(data.users);
+
+          console.log(data);
+        })
+
+        .catch((error) => console.log(error));
+    })
+
+    .catch((error) => console.log(error));
+}
+
+// fonction pour afiicher la liste desd message entre 2 users
+
+function printMessage(list) {
+  getListMessage.forEach(element => {
+    document.getElementById("discution").innerHTML = "";
+    // pn crée une div et un patragraphe 
+    let div = document.createElement("div");
+
+ let p = document.createElement("p");
+// on ajopute au paragraphe a la div
+    div.append(p);
+    // on ajoute au paragraphe son gtext
+    p.textContent = element.message;
+    if (element.expeditor == localStorage.getItem("iduser")) {
+      div.className = "expediteur";
+
+    } else {
+      div.className = "expediteur"
+    }
+    $("#discution").append(div);
+  })
+
 }
